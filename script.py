18abfd2c27e81bfa9bb2cfdee50bdf93af4ea433
Tomas <xxxxx@xxxx.com>
2019-09-10 19:57:52 -0300

    .

--- script.py
+++ script.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-
-#   twitter api
+#   TWITTER API
 consumer_key = config.consumer_key
 consumer_secret = config.consumer_secret 
 access_token = config.access_token
@@ -12,8 +11,6 @@ access_secret = config.access_secret
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)    
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
-
-
 
 def get_data():
     response = requests.get(config.url)
@@ -41,45 +38,60 @@ def save_data(disp, bloq):
     return True
 
 def get_graph():
+
+    #extract from csv
     d = pd.read_csv('data.csv')
-    time = d['TIME']
+    time = d['TIME'] 
     disp = d['DISPONIBLE']
     bloq = d['BLOQUEADA']
+
     plt.plot(time, disp)
     plt.plot(time, bloq)
-    plt.xlabel('Tiempo')
+    plt.xlabel('Tiempo desde 7/9/19')
     plt.ylabel('Bicicletas')
     plt.legend(["Bloqueadas", "Disponibles"])
-    plt.xticks(time, ['5/9/2019'])
+    plt.xticks([])
     plt.savefig('plot.png')
 
     return 'plot.png'
 
-last_id = 1170111344563097600
-
-while True:
-
-    bloq, disp = get_data()
-    #api.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
-    save_data(disp, bloq)
-
+def post_graph():
+    global last_id
     mentions = api.mentions_timeline(last_id, tweet_mode='extended')
     print(last_id)
-    for mention in reversed(mentions):
+    for mention in reversed(mentions): #parse mention from last to first
         print(str(mention.id) + " -- " + mention.full_text)
+        
         last_id = mention.id
 
-        if 'grafico!' in mention.full_text.lower():
+        if 'gráfico!' in mention.full_text.lower(): #if the mention has grafico...
             print("mentioned in a tweet!")
             msg = "@" + mention.user.screen_name 
-            api.update_with_media(get_graph(),msg, mention.id )
+            api.update_with_media(get_graph(),msg, mention.id ) #post tweet with graph
+    
+    return True
  
-   #dev note 1170111344563097600
 
-    time.sleep(600) #espera 10 minutos
+ 
+#1171523697011638284
+
+last_id = 1171523697011638284
+
+def main():
+    while True:
+
+        bloq, disp = get_data()
+        api.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
+        save_data(disp, bloq)
+
+        post_graph()
+
+        time.sleep(900) #waits 15 minutes
 
 
 
+if __name__ == "__main__":
+    main()
 
     
    
