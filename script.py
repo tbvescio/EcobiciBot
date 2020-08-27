987ac6797aa2c3f0ac528e92596031c0f3d4a3e4
tomas <tbvescio@gmail.com>
2020-08-27 12:39:48 -0300

    fixes

--- script.py
+++ script.py
@@ -1,7 +1,5 @@
-import requests, tweepy, time, config, datetime, csv
-import matplotlib.pyplot as plt
-import pandas as pd
-
+import time, config, datetime, csv
+import requests, tweepy
 
 #   TWITTER API
 consumer_key = config.consumer_key
@@ -11,6 +9,8 @@ access_secret = config.access_secret
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)    
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
+
+INTERVAL = 900
 
 def get_data():
     response = requests.get(config.url)
@@ -26,68 +26,19 @@ def get_data():
 
     return bloq, disp
 
-def save_data(disp, bloq):
-    timestamp = datetime.datetime.now()
-    data = [timestamp, disp, bloq]
-
-    with open('data.csv', 'a') as csvFile:
-        writer = csv.writer(csvFile)
-        writer.writerow(data)
-    csvFile.close()
-
-    return True
-
-def get_graph():
-
-    #extract from csv
-    d = pd.read_csv('data.csv')
-    time = d['TIME'] 
-    disp = d['DISPONIBLE']
-    bloq = d['BLOQUEADA']
-
-    plt.plot(time, disp)
-    plt.plot(time, bloq)
-    plt.xlabel('Tiempo desde 7/9/19')
-    plt.ylabel('Bicicletas')
-    plt.legend(["Bloqueadas", "Disponibles"])
-    plt.xticks([])
-    plt.savefig('plot.png')
-
-    return 'plot.png'
-
-def post_graph():
-    global last_id
-    mentions = api.mentions_timeline(last_id, tweet_mode='extended')
-    print(last_id)
-    for mention in reversed(mentions): #parse mention from last to first
-        print(str(mention.id) + " -- " + mention.full_text)
-        
-        last_id = mention.id
-
-        if 'gráfico!' in mention.full_text.lower(): #if the mention has grafico...
-            print("mentioned in a tweet!")
-            msg = "@" + mention.user.screen_name 
-            api.update_with_media(get_graph(),msg, mention.id ) #post tweet with graph
-    
-    return True
- 
-
- 
-#1171523697011638284
-
-last_id = 1171523697011638284
-
 def main():
     while True:
-
-        bloq, disp = get_data()
-        api.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
-        save_data(disp, bloq)
-
-        post_graph()
-
-        time.sleep(900) #waits 15 minutes
-
+        try:
+            bloq, disp = get_data()
+            print(bloq)
+            print(disp)
+            api.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
+            save_data(disp, bloq)
+            time.sleep(INTERVAL) #waits 15 minutes
+        except KeyboardInterrupt:
+            exit()
+        except:
+            print("error")
 
 
 if __name__ == "__main__":
