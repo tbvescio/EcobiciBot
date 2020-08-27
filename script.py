6dcdfcbe14233c46c2803140f60ccf2e869d40f7
tomas <tbvescio@gmail.com>
2020-08-27 12:51:34 -0300

    heroku env variables

--- script.py
+++ script.py
@@ -1,29 +1,28 @@
-import time, config, datetime, csv
+import time, config, datetime, csv, os
 import requests, tweepy
 
 #   TWITTER API
-consumer_key = config.consumer_key
-consumer_secret = config.consumer_secret 
-access_token = config.access_token
-access_secret = config.access_secret
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)    
-auth.set_access_token(access_token, access_secret)
-api = tweepy.API(auth)
+CONSUMER_KEY = os.environ['consumer_key']
+CONSUMER_SECRET = os.environ['consumer_secret'] 
+ACCESS_TOKEN = os.environ['access_token'] 
+ACCESS_SECRET = os.environ['access_secret'] 
+URL = os.environ['url'] 
+AUTH = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)    
+AUTH.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+API = tweepy.API(AUTH)
 
 INTERVAL = 900
 
 def get_data():
-    response = requests.get(config.url)
+    response = requests.get(URL)
     response = response.json()
 
     bloq = 0
     disp = 0
     for x in range(0, len(response["data"]["stations"])): # va de 0 a la cantidad de entradas de 'stations'
-
         #x se usa como indice para saber cual parsear 
         disp = disp + response["data"]["stations"][x]["num_bikes_available"] 
         bloq = bloq + response["data"]["stations"][x]["num_bikes_disabled"]
-
     return bloq, disp
 
 def main():
@@ -32,7 +31,7 @@ def main():
             bloq, disp = get_data()
             print(bloq)
             print(disp)
-            api.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
+            API.update_status(f"Disponibles: {disp} Bloquedas: {bloq}")
             save_data(disp, bloq)
             time.sleep(INTERVAL) #waits 15 minutes
         except KeyboardInterrupt:
